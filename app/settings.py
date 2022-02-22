e2c3480e5414584add8e178a1402547976c582f3
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-02-22 18:07:42 +0300

    configuring smtp

--- app/settings.py
+++ app/settings.py
@@ -128,3 +128,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authe.User'
+
+#smtp configurations
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'klausongudy@gmail.com'
+EMAIL_HOST_PASSWORD = 'gudyklauson12'
+EMAIL_USE_TLS = True
