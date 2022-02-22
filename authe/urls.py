c1295a76786070500ece955ad3fa5a43b6c9b7de
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-02-22 18:11:12 +0300

    function based views during password reset

--- authe/urls.py
+++ authe/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('login/', views.loginpage, name='login'),
     path('register/', views.registerpage, name='register'),
@@ -8,4 +10,13 @@ urlpatterns = [
 
     # should be moved to task
     path('', views.home, name='home'),
+
+    #password reset 
+    path('reset_password/',
+    auth_views.PasswordResetView.as_view(template_name='authe/password_reset.html'),
+    name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='authe/password_reset_sent.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authe/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authe/password_reset_done.html'), name='password_reset_complete'),
+    
 ]
