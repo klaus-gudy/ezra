eab39849a933da206ce800b746d5e88c34769079
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-03-02 17:11:04 +0300

    sovling static files and root

--- app/settings.py
+++ app/settings.py
@@ -136,10 +136,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+if DEBUG:
+    STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
